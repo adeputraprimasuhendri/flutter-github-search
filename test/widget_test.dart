89561935dd93c537a0621a2fd482232cf0985c03
indosiana <adeputra.mtech@gmail.com>
2021-05-12 11:03:10 +0700

    Add Test -

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,10 +6,25 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_github_search/main.dart';
 import 'package:flutter_github_search/models/search_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  testWidgets('Search github user', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await tester.pump();
+    expect(find.byType(TextFormField), findsOneWidget);
+
+    final Finder textinput = find.byType(TextFormField);
+    await tester.tap(textinput);
+    await tester.showKeyboard(find.byType(TextFormField));
+    await tester.enterText(find.byType(TextFormField), 'ade');
+    await tester.pump();
+    expect(find.text('ade'), findsOneWidget);
+  });
+
   test('Parse json over a network', () async {
     final url = 'https://api.github.com/search/users?q=ade&per_page=10';
     final res = await Dio().get(url);
