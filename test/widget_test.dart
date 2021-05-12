6d22a4a560b3da91cf0f724b2aba042ef3f72eeb
indosiana <adeputra.mtech@gmail.com>
2021-05-12 09:54:08 +0700

    Add Test - Parse json over network -

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_github_search/models/search_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_github_search/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Parse json over a network', () async {
+    final url = 'https://api.github.com/search/users?q=ade&per_page=10';
+    final res = await Dio().get(url);
+    if (res.statusCode == 200) {
+      expect(SearchModel.fromJson(res.data), isNotNull);
+    }
   });
 }
