d1d4edd6f453df02e1608ecc7a35783ad405eb47
indosiana <adeputra.mtech@gmail.com>
2021-05-12 09:56:59 +0700

    Add Test - Parse an item json over network -

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -17,4 +17,12 @@ void main() {
       expect(SearchModel.fromJson(res.data), isNotNull);
     }
   });
+
+  test('Parse an item json over a network', () async {
+    final url = 'https://api.github.com/search/users?q=ade&per_page=10';
+    final res = await Dio().get(url);
+    if (res.statusCode == 200) {
+      expect(SearchModel.fromJson(res.data).items.first.login, 'ade');
+    }
+  });
 }
